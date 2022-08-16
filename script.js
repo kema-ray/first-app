5bc3bfefdd02746dc469aa6c20a2ce281ae69f92
rachel <rachelkemuma99@gmail.com>
2022-08-16 16:11:22 +0300

    add vue cdn

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const buttonEl = document.querySelector('button');
-const inputEl = document.querySelector('input');
-const listEl = document.querySelector('ul');
+// Imperative approach
+// const buttonEl = document.querySelector('button');
+// const inputEl = document.querySelector('input');
+// const listEl = document.querySelector('ul');
 
-function addGoal() {
-  const enteredValue = inputEl.value;
-  const listItemEl = document.createElement('li');
-  listItemEl.textContent = enteredValue;
-  listEl.appendChild(listItemEl);
-  inputEl.value = '';
-}
+// function addGoal() {
+//   const enteredValue = inputEl.value;
+//   const listItemEl = document.createElement('li');
+//   listItemEl.textContent = enteredValue;
+//   listEl.appendChild(listItemEl);
+//   inputEl.value = '';
+// }
 
-buttonEl.addEventListener('click', addGoal);
+// buttonEl.addEventListener('click', addGoal);
